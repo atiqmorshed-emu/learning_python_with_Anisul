42ed0c5a8358151dea472790310bdaf8461cb451
Your Name <your@email.com>
2026-02-05 00:01:10 +0600

    learning operation

--- Variables/Learning_variable.py
+++ Variables/Learning_variable.py
@@ -1,2 +1,18 @@
 name= "Md. Atiq morshed Emu"
 print(name)
+
+number=10
+print(number)
+
+
+
+# addition
+number1 = 5
+number2 = 50
+result = number1+number2
+print(result)
+
+# substraction
+
+result2 = number1 - number2
+print(result2)
