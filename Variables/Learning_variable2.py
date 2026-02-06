373e1fa85b3e1bce41df85acca7d8556699c24c4
Your Name <your@email.com>
2026-02-07 02:24:13 +0600

    connect string with another string

---
+++ Variables/Learning_variable2.py
@@ -0,0 +1,15 @@
+name = "Atiq Morshed Emu"
+age = 27
+cgpa = 3.07
+
+print(name)
+print(cgpa)
+print(age)
+
+#connect string with another string
+
+print("Our new student name is "+name)
+print(name+ " lives in dhaka")
+print(name+ " is currently 27 years old")
+print(name+ " At the age of 27, he has started to learn python")
+print(name+ " has scored 3.07 in his secondary exam")
